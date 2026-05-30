ef1f29351fa6247839e955ff31e3a998e6dcb2db
IkwueInalegwu <ikwueinalegwu@gmail.com>
2026-05-30 11:53:47 +0100

    init of descartes project for AxisMarketPartners

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+import { Schema } from "effect";
+
+export class NewsItem extends Schema.Class<NewsItem>("NewsItem")({
+	title: Schema.String,
+	url: Schema.String,
+	publishedAt: Schema.String,
+	description: Schema.String,
+	source: Schema.String,
+}) {}
+
+export interface SourceConfig {
+	name: string;
+	type: "rss" | "json";
+	url: string;
+}
