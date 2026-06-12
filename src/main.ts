6d297d0d5302fef11bb9f2cc0ff84d2597a8cb22
IkwueInalegwu <ikwueinalegwu@gmail.com>
2026-06-12 12:07:52 +0100

    adding tailwind

--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,16 @@ app.use('*', async (c, next) => {
     return c.html(
       `<html>
     <head>
-    <title></title>
+    <title>Descartes Preview</title>
+      <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     </head>
-    <body>${content}</body>
+    <body class="bg-neutral-100 text-neutral-950 overflow-hidden">
+      <div className="w-3/6 mx-auto h-screen overflow-hidden overflow-y-scroll">
+      ${content}
+      </div>
+    </body>
     </html>`,
     );
   });
